aedd790c06178960e9edc0353d84297583c004ba
my name <replacement2419@gmail.com>
2024-12-07 18:10:12 -0500

    Update install.sh
    it would try to install qt5 shared libraries for you

--- bin/install.sh
+++ bin/install.sh
@@ -6,4 +6,5 @@ touch ~/.local/share/applications/Zira.desktop
 sed "s|%dir%|$PWD|g" desktop.txt > ~/.local/share/applications/Zira.desktop
 temp=$(dirname $(realpath "$0"))
 chmod 777 $temp/ZiraEditor
+sudo apt-get install qt5-default
 exec $temp/ZiraEditor
